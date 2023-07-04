bfbc2a2489f5586717453f6f481fea4e2df01c3d
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-07-04 15:43:44 -0500

    Error page added

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,11 @@ const router = createRouter({
       path: '/portfolio',
       name: 'portfolio',
       component: () => import('../views/PortfolioView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/ErrorView.vue')
     }
   ]
 })
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="img-container">
+            <img src="../../public/ErrorImg.jpg" alt="Baby duck" class="error-img">
+        </div>
+        <div class="heading-container">
+            <h4 class="title">Page not found</h4>
+            <p class="content">
+                Maybe check that your url is correct..
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.img-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.error-img {
+    margin: 0 auto;
+    height: 400px;
+    width: 450px;
+    object-fit: scale-down;
+}
+
+.title {
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: .5rem;
+}
+
+.content {
+    text-align: center;
+}
+
+@media (max-width: 492px) {
+    .error-img {
+        width: 100%;
+    }
+}
+</style>
